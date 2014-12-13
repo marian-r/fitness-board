5361452b765ba6dc4e5cb90a478eb31975eefe2e
Marian Rusnak <grizly21@gmail.com>
2014-12-13 01:43:58 +0100

    Added medical data

--- out/scripts/main.js
+++ out/scripts/main.js
@@ -7,6 +7,7 @@ System.registerModule("../../scripts/main.js", [], function() {
   var username = "ois.seminar";
   var password = "ois4fri";
   var users = [];
+  var selectedUser = {};
   function getSessionId() {
     var response = $.ajax({
       type: "POST",
@@ -16,9 +17,11 @@ System.registerModule("../../scripts/main.js", [], function() {
     return response.responseJSON.sessionId;
   }
   function generate() {
-    var $__0 = function(i, len) {
+    var $__1 = function(i, len) {
       var user = users[i];
+      var medicalData = user.medicalData;
       delete user.ehrId;
+      delete user.medicalData;
       $.ajax({
         url: baseUrl + "/ehr",
         type: 'POST',
@@ -38,6 +41,15 @@ System.registerModule("../../scripts/main.js", [], function() {
               if (party.action === 'CREATE') {
                 console.log("Created EHR '" + ehrId + "'.");
                 users[i].ehrId = ehrId;
+                for (var j = 0,
+                    len = medicalData.length; j < len; j++) {
+                  var $__0 = medicalData[j],
+                      dateTime = $__0.dateTime,
+                      bodyWeight = $__0.bodyWeight,
+                      pulse = $__0.pulse,
+                      bodyTemperature = $__0.bodyTemperature;
+                  createMedicalData(bodyWeight, pulse, bodyTemperature, dateTime, ehrId);
+                }
               }
             },
             error: function(err) {
@@ -49,12 +61,73 @@ System.registerModule("../../scripts/main.js", [], function() {
     };
     for (var i = 0,
         len = users.length; i < len; i++) {
-      $__0(i, len);
+      $__1(i, len);
     }
+  }
+  function createMedicalData(bodyWeight, pulse, bodyTemperature, dateTime, ehrId) {
+    var data = {
+      "ctx/language": "en",
+      "ctx/territory": "SI",
+      "ctx/time": dateTime
+    };
+    if (bodyWeight) {
+      data['vital_signs/body_weight/any_event/body_weight'] = bodyWeight;
+    }
+    if (pulse) {
+      data["vital_signs/body_temperature/any_event/temperature|magnitude"] = bodyTemperature;
+      data["vital_signs/body_temperature/any_event/temperature|unit"] = '°C';
+    }
+    if (bodyTemperature) {
+      data["vital_signs/pulse/any_event/rate|magnitude"] = pulse;
+      data["vital_signs/pulse/any_event/rate|unit"] = "/min";
+    }
+    var params = {
+      "ehrId": ehrId,
+      templateId: 'Vital Signs',
+      format: 'FLAT'
+    };
+    $.ajax({
+      url: baseUrl + "/composition?" + $.param(params),
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: function(res) {
+        console.log(res.meta.href);
+      },
+      error: function(err) {
+        console.log(JSON.parse(err.responseText).userMessage);
+      }
+    });
+  }
+  function submitForm() {
+    var ehrId = selectedUser.ehrId;
+    var dateTime = new Date();
+    var bodyWeight = $("#inputWeight").val();
+    var pulse = $("#inputPulse").val();
+    var bodyTemperature = $("#inputTemperature").val();
+    if (!bodyWeight && !pulse && !bodyTemperature) {
+      console.log("Required");
+    }
+    createMedicalData(bodyWeight, pulse, bodyTemperature, dateTime, ehrId);
   }
   function loadPatients() {
     $.getJSON('data/patients.json', function(data) {
       users = data;
+      selectedUser = users[0];
+      getWeights();
+    });
+  }
+  function getWeights() {
+    var ehrId = selectedUser.ehrId;
+    $.ajax({
+      url: baseUrl + "/view/" + ehrId + "/" + "weight",
+      type: 'GET',
+      success: function(res) {
+        console.log(res);
+      },
+      error: function() {
+        console.log(JSON.parse(err.responseText).userMessage);
+      }
     });
   }
   $(function() {
@@ -64,6 +137,7 @@ System.registerModule("../../scripts/main.js", [], function() {
     $('#linkShow').click(function() {
       console.log(users);
     });
+    $('#submitButton').click(submitForm);
   });
   return {};
 });
--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ var username = "ois.seminar";
 var password = "ois4fri";
 
 var users = [];
+var selectedUser = {};
 
 function getSessionId() {
     var response = $.ajax({
@@ -22,7 +23,10 @@ function generate() {
 
     for (let i = 0, len = users.length; i < len; i++) {
         let user = users[i];
+        let medicalData = user.medicalData;
+
         delete user.ehrId;
+        delete user.medicalData;
 
         $.ajax({
             url: baseUrl + "/ehr",
@@ -42,6 +46,12 @@ function generate() {
                         if (party.action === 'CREATE') {
                             console.log("Created EHR '" + ehrId + "'.");
                             users[i].ehrId = ehrId;
+
+                            for (let j = 0, len = medicalData.length; j < len; j++) {
+
+                                var { dateTime, bodyWeight, pulse, bodyTemperature } = medicalData[j];
+                                createMedicalData(bodyWeight, pulse, bodyTemperature, dateTime, ehrId);
+                            }
                         }
                     },
                     error: function(err) {
@@ -53,9 +63,82 @@ function generate() {
     }
 }
 
+function createMedicalData(bodyWeight, pulse, bodyTemperature, dateTime, ehrId) {
+    var data = {
+        // Preview Structure: https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
+        "ctx/language": "en",
+        "ctx/territory": "SI",
+        "ctx/time": dateTime
+    };
+
+    if (bodyWeight) {
+        data['vital_signs/body_weight/any_event/body_weight'] = bodyWeight;
+    }
+    if (pulse) {
+        data["vital_signs/body_temperature/any_event/temperature|magnitude"] = bodyTemperature;
+        data["vital_signs/body_temperature/any_event/temperature|unit"] = '°C';
+    }
+    if (bodyTemperature) {
+        data["vital_signs/pulse/any_event/rate|magnitude"] = pulse;
+        data["vital_signs/pulse/any_event/rate|unit"] = "/min";
+    }
+
+
+    var params = {
+        "ehrId": ehrId,
+        templateId: 'Vital Signs',
+        format: 'FLAT'
+    };
+
+    $.ajax({
+        url: baseUrl + "/composition?" + $.param(params),
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function (res) {
+            console.log(res.meta.href);
+        },
+        error: function (err) {
+            console.log(JSON.parse(err.responseText).userMessage);
+        }
+    });
+}
+
+function submitForm() {
+    var ehrId = selectedUser.ehrId;
+    var dateTime = new Date(); // current time
+    var bodyWeight = $("#inputWeight").val();
+    var pulse = $("#inputPulse").val();
+    var bodyTemperature = $("#inputTemperature").val();
+
+    if (!bodyWeight && !pulse && !bodyTemperature) {
+        console.log("Required");
+    }
+
+    createMedicalData(bodyWeight, pulse, bodyTemperature, dateTime, ehrId);
+}
+
 function loadPatients() {
     $.getJSON('data/patients.json', function (data) {
         users = data;
+        selectedUser = users[0];
+
+        getWeights();
+    });
+}
+
+function getWeights() {
+    var ehrId = selectedUser.ehrId;
+
+    $.ajax({
+        url: baseUrl + "/view/" + ehrId + "/" + "weight",
+        type: 'GET',
+        success: function (res) {
+            console.log(res);
+        },
+        error: function() {
+            console.log(JSON.parse(err.responseText).userMessage);
+        }
     });
 }
 
@@ -70,4 +153,6 @@ $(function () {
     $('#linkShow').click(function () {
         console.log(users);
     });
+
+    $('#submitButton').click(submitForm);
 });
